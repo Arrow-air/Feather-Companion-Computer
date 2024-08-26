7cc45d34d61e2b513e169b4285796f7370e85442
Alexander Dada <akinola@singerinstruments.com>
2024-08-26 14:59:19 +0100

    Update VESCCAN.py

--- src/VESCCAN.py
+++ src/VESCCAN.py
@@ -259,6 +259,7 @@ if __name__ == "__main__":
 
             #print(vesc.unitData)
             #print("\n")
+            print(len(vesc.unitData['6']))
 
         print(vesc.unitData)
         print("\n")
