d98a0abde851a113d8087d7c61d160db959157ef
Alexander Dada <akinola@singerinstruments.com>
2024-08-26 14:03:18 +0100

    Update VESCCAN.py

--- src/VESCCAN.py
+++ src/VESCCAN.py
@@ -194,7 +194,7 @@ class VESCCAN:
             self.msgData['NoOfCells'] = data[0] #struct.unpack('<B', data[0:1])[0]
             data2 = list(data[1:8])
             data2.append(0)
-            self.msgData['bal_state'] = struct.unpack('>Q', bytes(data2))[0] #struct.unpack('<Q', data2)[0] >> 1
+            #self.msgData['bal_state'] = struct.unpack('>Q', bytes(data2))[0] #struct.unpack('<Q', data2)[0] >> 1
 
         elif command == 11530 or command == 46:#0x2D1A:  # CAN_PACKET_BMS_SOC_SOH_TEMP_STAT
             #print(command)
