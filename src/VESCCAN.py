10579330a8b53f8de97c925d8f028122673869bd
Alexander Dada <akinola@singerinstruments.com>
2024-10-14 23:00:32 +0100

    Update VESCCAN.py

--- src/VESCCAN.py
+++ src/VESCCAN.py
@@ -260,7 +260,7 @@ if __name__ == "__main__":
                 vesc.unitData['1'] |= rawData
             elif rawData['unit_id'] == 12:
                 vesc.unitData['2'] |= rawData
-            elif rawData['unit_id'] == 12:
+            elif rawData['unit_id'] == 1:
                 vesc.unitData['3'] |= rawData
             elif rawData['unit_id'] == 14:
                 vesc.unitData['4'] |= rawData
