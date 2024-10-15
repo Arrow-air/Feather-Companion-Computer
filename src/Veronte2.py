5d7f402b145cf3df49f47fac6f86c3d998a720b2
Alexander Dada <akinola@singerinstruments.com>
2024-10-15 22:03:54 +0100

    Update Veronte2.py

--- src/Veronte2.py
+++ src/Veronte2.py
@@ -125,7 +125,7 @@ class Veronte2:
                     for i in range((data_length // 4)):  # Adjust as per actual format
                         variable_bytes = self.VeronteSerial.read(4)
                         variable = self.unpack_mixed_endian_float(variable_bytes)
-                        telemetry_data.append({f"Variable{i}": variable})
+                        telemetry_data.append({f"Variable{i}": round(variable,3)})
 
                     packet['data'] = telemetry_data
 
