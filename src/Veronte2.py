226efe339aeba381078da0dbeeb213bb43847772
Alexander Dada <akinola@singerinstruments.com>
2024-10-16 20:11:20 +0100

    Update Veronte2.py

--- src/Veronte2.py
+++ src/Veronte2.py
@@ -101,6 +101,7 @@ class Veronte2:
                     # Read variables (XTYPE, assuming float32 for example)
                     print(data_length // 4)
                     for i in range((data_length // 4)):
+                        print(i)
                         variable_bytes = self.VeronteSerial.read(4)
                         variable = self.unpack_mixed_endian_float(variable_bytes)
                         telemetry_data.append({f"Variable{i}": round(variable, 2)})
